fa0956f5f11d99741000415af08e9505e261617a
Jeffery <i@youjf.com>
2012-08-10 16:22:09 +0800

    updated config sample

--- config/config.sample.php
+++ config/config.sample.php
@@ -6,11 +6,10 @@ $root =  str_replace("\\", "/", dirname(__FILE__)) . '/../';
 
 $site = array(
     'FOLDER_ROOT' => $root,
-    'BASE_URI' => 'http://sgol.sinaapp.com/',
     // use IP as the host, it will cause connection error if use localhost
-    'DATABASE_DSN' => "mysql:dbname=" . SAE_MYSQL_DB . ";host=" . SAE_MYSQL_HOST_M . ";port=" . SAE_MYSQL_PORT,
-    'DATABASE_USER' => SAE_MYSQL_USER,
-    'DATABASE_PASSWD' => SAE_MYSQL_PASS,
+    'DATABASE_DSN' => "mysql:dbname=sgol;host=127.0.0.1",
+    'DATABASE_USER' => "root",
+    'DATABASE_PASSWD' => "",
 	'CHARSET' => 'UTF-8',
 	'debug' => false,
 );
